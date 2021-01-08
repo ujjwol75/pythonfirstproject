d5bd8c4ee65634f16eb646f95dde8a54c5f80675
ujjwol75 <ujjwolneupane75@gmail.com>
2021-01-08 09:30:24 +0545

    initial commit

--- tictactoegame.py
+++ tictactoegame.py
@@ -1,59 +1,3 @@
-# def divide(a, b):
-#     try:
-    
-#         assert b != 0, "You cannot give denomenator 0."
-#         return a/b
-#     except:
-#         print("Error message, divide by 0")
-
-# div = divide(2, 2)
-# print(div)
-
-# import copy
-# list1 = [1,3, 4, 5, 6, 43]
-# list2 = copy.copy(list1)
-# print(list2)
-
-# class demo:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
-
-#     def __repr__(self):
-#         return '__repr__ for demo'
-    
-#     def __str__(self):
-#         return "__str__for demo"
-
-# o = demo(1, 2)
-
-# print(str(o))
-# print(repr(o))
-
-
-# pip install virtualenv
-# virtualenv <name>
-# virtualenv activated
-
-
-
-# import pygame
-# background_colour = (255,255,255)
-# (width, height) = (300, 200)
-# screen = pygame.display.set_mode((width, height))
-# pygame.display.set_caption('Tutorial 1')
-# screen.fill(background_colour)
-# pygame.display.flip()
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             running = False 
-# import numpy as np
-# array = np.array([[0,1,3],[9,8,7],[8,3,2]])
-# print(array)
-
 board = [['_', '_', '_'],
          ['_', '_', '_'],
          ['_', '_', '_']]
@@ -71,7 +15,11 @@ def print_board(board):
 def user_input():
     pos = input("\n Enter any number from 1 to 9: ")
     pos = int(pos) -1
-    return pos
+    if pos <= 9:
+        return pos
+    else:
+        pos = input("\n Enter any number from 1 to 9: ")
+        pos = int(pos) -1
 
 # position of the player input in board
 def place_player(pos, player):
@@ -79,6 +27,8 @@ def place_player(pos, player):
     column = pos % 3
     board[row][column] = player
     return 1
+    
+        
 
 # check if game tie 
 def game_tie():
@@ -161,9 +111,9 @@ def main():
 
     while game_on:
         if counter % 2 == 0:
-            player = 'x'
-        else:
             player = 'y'
+        else:
+            player = 'x'
 
         
         pos = user_input()
@@ -173,7 +123,7 @@ def main():
             game_on = check_column(j)
             game_on = check_row(j)
         check_diagonal(board[0][0], board[1][1], board[2][2])
-        check_diagonal(board[0][2], board[1][1], board[0][2])
+        check_diagonal(board[0][2], board[1][1], board[2][0])
         
         print_board(board)
         game_tie()
